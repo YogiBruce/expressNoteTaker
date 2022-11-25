7d7b627f0430dfe8861e996facf5a140603b03f6
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-25 14:18:48 -0800

    index routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
-const notes = require('express').router()
+const express = require('express');
+
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', noteRouter);
+
+module.exports = app;
