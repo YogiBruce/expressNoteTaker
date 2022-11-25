ebc81fc03ff2997344699d1c53f58d38e90a3fbc
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 17:15:11 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,65 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Parse incoming array data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", api);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully posted note")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response)
+  } else {
+    res.status(500).json('Error posting note')
+  }
+});
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
